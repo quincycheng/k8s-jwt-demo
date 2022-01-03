35d7ecec563c43b147b08aeaa81dd41934c23173
Quincy Cheng <quincy.cheng@gmail.com>
2022-01-03 16:41:37 +0800

    Add more comments to scripts

--- bin/5-patch-conjur-k8s.sh
+++ bin/5-patch-conjur-k8s.sh
@@ -3,8 +3,10 @@
 SCRIPT_ROOT=$(dirname "$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )")
 source ${SCRIPT_ROOT}/conf/conjur.conf
 
-# Trust Kubernetes CA cert
+# Trust Kubernetes CA cert, default: /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
 kubectl set env deployment/conjur-oss -n conjur-server SSL_CERT_FILE=${JWT_CA_PATH}
 
 # Allow access to JWKS 
-kubectl create clusterrolebinding oidc-reviewer --clusterrole=system:service-account-issuer-discovery --group=system:unauthenticated
+kubectl create clusterrolebinding oidc-reviewer \
+  --clusterrole=system:service-account-issuer-discovery \
+  --group=system:unauthenticated
--- bin/88-test-query.sh
+++ bin/88-test-query.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 SCRIPT_ROOT=$(dirname "$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )")
 source ${SCRIPT_ROOT}/conf/conjur.conf
 
